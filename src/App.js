1ff0d4459528dcf37147790da6105e96cbf7ceca
Jan Odenkirchen <j-f-odenkirchen@t-online.de>
2021-07-13 13:34:35 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+//script imports
+import React from 'react';
+import Logo from './logo';
+//css imports
+import './main.css';
+import './introLogo.css';
+import './logochecker';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+  return(
+    <div>
+      <Logo />
     </div>
+    
   );
 }
-
 export default App;
