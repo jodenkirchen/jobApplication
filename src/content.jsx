79278b24ac866d29fb58b1f01a4553869110cee1
Jan Odenkirchen <j-f-odenkirchen@t-online.de>
2022-03-04 12:56:30 +0100

    burgerBodyInitialCommit

--- src/content.jsx
+++ src/content.jsx
@@ -1,14 +1,23 @@
 import React, {useState} from 'react';
 function Content() {
-    const [active, setActive] = useState(false);
+    const [burgerActive, setBurgerActive] = useState(false);
+    
+  function  burgerSwap() {
+        document.body.classList.toggle = 'burgerActive';
+      }
 
     return (
         <div id="contentWrapper">
-            <div id="burgerMenu" onClick={() => setActive(!active)}>
-                <div id="firstLine" className={active? "line active" : "line"}></div>
-                <div id="secondLine" className={active? "line active" : "line"}></div>
-                <div id="thirdLine" className={active? "line active" : "line"}></div>
+            <div id="burgerMenu" onClick={() => { burgerSwap(); setBurgerActive(!burgerActive)}}>
+                <div id="firstLine" className={burgerActive? "line active" : "line"}></div>
+                <div id="secondLine" className={burgerActive? "line active" : "line"}></div>
+                <div id="thirdLine" className={burgerActive? "line active" : "line"}></div>
             </div>
+            <nav className={burgerActive? "active" : null}>
+                <li className="navItem"><span>Home</span></li>
+                <li className="navItem"><span>About</span></li>
+                <li className="navItem"><span>Shop</span></li>
+            </nav>
         </div>
     )
 }
