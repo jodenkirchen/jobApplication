e11259b0f1890cb95c2766a187b2edead1c7476a
Jan Odenkirchen <j-f-odenkirchen@t-online.de>
2022-03-30 11:21:47 +0200

    TimelineFrame

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Logo from './logo';
 import Content from './content.jsx';
 import Skills from './skills.jsx';
 import Navigation from './navigation.jsx';
+import Timeline from './timeline.jsx';
 //css imports
 import './main.css';
 import './introLogo.css';
@@ -18,6 +19,7 @@ function App(){
       <Navigation />
       <Content />
       <Skills />
+      <Timeline />
     </div>
     
   );
---
+++ src/timeline.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+function Timeline() {
+    return (  
+        <div id="timelineWrapper" className="introHidden section">
+            <div className="tlContainer">
+                <div className="tlContent tlLeft">
+                    2014
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Timeline;
