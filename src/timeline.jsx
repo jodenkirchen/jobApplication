47e1404e5c532b0c0c8e21a7f376ae2e5f605711
Jan Odenkirchen <j-f-odenkirchen@t-online.de>
2022-04-04 15:04:18 +0200

    timelineContentLayoutUpdate

--- src/timeline.jsx
+++ src/timeline.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 function Timeline() {
-    return (  
+    return (
         <div id="timelineWrapper" className="introHidden section">
             <div className="tlContainer">
-                <div className="tlContent tlLeft">
+                <div id="tlFirst" className="tlContent tlLeft tlTime">
                     2014
                 </div>
+                <div className="tlTextRight">
+                    <span className="tlTime">Vocational School Degree</span><br />Software Development
+                    <br />
+                    <br />
+                    <span className="tlTime">Software Developer</span>,<br />SMS Meer GmbH,<br />M&ouml;nchengladbach,<br />&asymp; 14,000 staff members
+                </div>
             </div>
+
         </div>
     );
 }
