47389c0fb62d94c4987733db34d364ac1cbe5b26
Jan Odenkirchen <j-f-odenkirchen@t-online.de>
2022-04-06 12:02:51 +0200

    TimelineFinalContent

--- src/timeline.jsx
+++ src/timeline.jsx
@@ -3,14 +3,38 @@ function Timeline() {
     return (
         <div id="timelineWrapper" className="introHidden section">
             <div className="tlContainer">
-                <div id="tlFirst" className="tlContent tlLeft tlTime">
+                <div className="tlContent tlLeft tlTime">
                     2014
                 </div>
                 <div className="tlTextRight">
                     <span className="tlTime">Vocational School Degree</span><br />Software Development
                     <br />
                     <br />
-                    <span className="tlTime">Software Developer</span>,<br />SMS Meer GmbH,<br />M&ouml;nchengladbach,<br />&asymp; 14,000 staff members
+                    <span className="tlTime">Software Developer</span>&#044;<br />SMS Meer GmbH&#044;<br />M&ouml;nchengladbach&#044;<br />&asymp; 14&#044;000 staff members
+                </div>
+            </div>
+            <div className="tlContainer">
+                <div className="tlContent tlLeft tlTime">
+                    2018
+                </div>
+                <div className="tlTextRight">
+                    <span className="tlTime">Frontend Developer</span>&#044;<br />kasse 9 interaktiv e&#046;K&#046;&#044;<br />Essen&#044;<br />&asymp; 20 staff members
+                </div>
+            </div>
+            <div className="tlContainer">
+                <div className="tlContent tlLeft tlTime">
+                    2019
+                </div>
+                <div className="tlTextRight">
+                    <span className="tlTime">Fullstack Developer</span>&#044;<br />Magna Steyr Fuel Systems GmbH&#044;<br />Grevenbroich&#044;<br />&asymp; 160&#044;000 staff members
+                </div>
+            </div>
+            <div className="tlContainer">
+                <div className="tlContent tlLeft tlTime">
+                    2021
+                </div>
+                <div className="tlTextRight">
+                    <span className="tlTime">Bachelor of Science, Data Science<br />&#040;In progress&#041;</span>,<br />International University of Applied Sciences,<br />Distance Learning
                 </div>
             </div>
 
